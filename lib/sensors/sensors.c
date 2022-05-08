e303eacca8712499a4e20612205a82bc711413ae
ShaneWaxler1 <swaxler@mail.sfsu.edu>
2022-05-07 19:13:38 -0700

    cleared comments

--- lib/sensors/sensors.c
+++ lib/sensors/sensors.c
@@ -17,9 +17,6 @@
 #include <pigpio.h>
 #include <pthread.h>
 
-#define PINOA 24 /* Obstacle Avoidance IR GPIO pin in */
-#define PINLS 23 /* Line Sensor GPIO pin in */
-
 void* sense(void* ptr)
 {
 	sensor* s = (struct sensor*) ptr;
@@ -27,44 +24,3 @@ void* sense(void* ptr)
 	while(1)
 		s->read = gpioRead(pin);
 }
-
-// int main()
-// {
-// 	if(gpioInitialise() < 0)
-// 	{
-// 		fprintf(stderr, "pigpio initialise failure\n");
-// 		return 1;
-// 	}
-	
-// 	gpioSetMode(PINOA, PI_INPUT);
-// 	gpioSetMode(PINLS, PI_INPUT);
-
-// 	pthread_t obstacle_thread, line_thread;
-// 	int obstacle_return, line_return;
-	
-// 	sensor obstacle, line;
-
-// 	obstacle.pin = PINOA;
-// 	obstacle.read = 0;
-
-// 	line.pin = PINLS;
-// 	line.read = 0;
-
-// 	obstacle_return = pthread_create(&obstacle_thread, NULL, sense, &obstacle);
-// 	line_return = pthread_create(&line_thread, NULL, sense, &line);
-	
-// 	while(1)
-// 	{
-// 		printf("Line read: %i\n", line.read);
-// 		printf("Obstacle read: %i\n\n", obstacle.read);
-// 		usleep(500000);
-// 	}
-
-// 	pthread_join(obstacle_thread, NULL);
-// 	pthread_join(line_thread, NULL);
-
-// 	printf("Obstacle thread returns: %i\n", obstacle_return);
-// 	printf("Line thread returns: %i\n", line_return);
-
-// 	return 0;
-// }

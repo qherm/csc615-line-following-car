5e0e04f90962523b0a3e4400d8058f2eba5ded99
ShaneWaxler1 <swaxler@mail.sfsu.edu>
2022-05-07 21:02:44 -0700

    motor test

--- main/main.c
+++ main/main.c
@@ -15,32 +15,34 @@ int main()
 	}
 	
 	gpioSetMode(IRL, PI_INPUT);
-  gpioSetMode(IRM, PI_INPUT);
+  	gpioSetMode(IRM, PI_INPUT);
 	gpioSetMode(IRR, PI_INPUT);
 
-  printf("left pin %d",IRL);
-  printf("right pin %d",IRR);
-  printf("middle pin %d",IRM);
+  	printf("left pin %d",IRL);
+  	printf("right pin %d",IRR);
+  	printf("middle pin %d",IRM);
 
 	pthread_t line_left_thread, line_middle_thread, line_right_thread, object_middle_thread;
 	int line_left_return, line_middle_return, line_right_return, object_middle_return;
 	
 	sensor line_right, line_middle, line_left, object_middle;
 
-  line_left.pin = IRL;
-  line_left.read = 0;
+  	line_left.pin = IRL;
+  	line_left.read = 0;
 
-  line_middle.pin = IRM;
-  line_middle.read = 0;
+	line_middle.pin = IRM;
+  	line_middle.read = 0;
 
 	line_right.pin = IRR;
 	line_right.read = 0;
 
 	line_left_return = pthread_create(&line_left_thread, NULL, sense, &line_left);
 	line_middle_return = pthread_create(&line_middle_thread, NULL, sense, &line_middle);
-  line_right_return = pthread_create(&line_right_thread, NULL, sense, &line_right);
+  	line_right_return = pthread_create(&line_right_thread, NULL, sense, &line_right);
 	
 	init_motors();
+
+	Motor_Run(MOTORA, FORWARD, 100);
 
 	while(1)
 	{

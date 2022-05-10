9d4a2e4c7767c6f2a192cc2f932161744619cba5
ShaneWaxler1 <swaxler@mail.sfsu.edu>
2022-05-09 17:26:25 -0700

    fixed direction and motor labels

--- main/main.c
+++ main/main.c
@@ -27,65 +27,65 @@ int main()
   	printf("right pin %d",IRR);
   	printf("middle pin %d",IRM);
 
-	// pthread_t line_left_thread, line_middle_thread, line_right_thread, object_middle_thread;
-	// int line_left_return, line_middle_return, line_right_return, object_middle_return;
+	pthread_t line_left_thread, line_middle_thread, line_right_thread, object_middle_thread;
+	int line_left_return, line_middle_return, line_right_return, object_middle_return;
 	
-	// sensor line_right, line_middle, line_left, object_middle;
+	sensor line_right, line_middle, line_left, object_middle;
 
-  	// line_left.pin = IRL;
-  	// line_left.read = 0;
+  	line_left.pin = IRL;
+  	line_left.read = 0;
 
-	// line_middle.pin = IRM;
-  	// line_middle.read = 0;
+	line_middle.pin = IRM;
+  	line_middle.read = 0;
 
-	// line_right.pin = IRR;
-	// line_right.read = 0;
+	line_right.pin = IRR;
+	line_right.read = 0;
 
-	// line_left_return = pthread_create(&line_left_thread, NULL, sense, &line_left);
-	// line_middle_return = pthread_create(&line_middle_thread, NULL, sense, &line_middle);
-  	// line_right_return = pthread_create(&line_right_thread, NULL, sense, &line_right);
+	line_left_return = pthread_create(&line_left_thread, NULL, sense, &line_left);
+	line_middle_return = pthread_create(&line_middle_thread, NULL, sense, &line_middle);
+  	line_right_return = pthread_create(&line_right_thread, NULL, sense, &line_right);
 
 	if(DEV_ModuleInit())
     	return 1;
 
 	Motor_Init();
 
-	Motor_Run(MOTORA, FORWARD, 100);
-	Motor_Run(MOTORB, FORWARD, 100);
+	Motor_Run(LEFT_MOTOR, FORWARD, 100);
+	Motor_Run(RIGHT_MOTOR, FORWARD, 100);
 	
 	while(1)
 	{
 		// LineSensor.read==1: sensor reads white
 		// LineSensor.read==0: sensor reads black
-		// if(line_left.read && line_middle.read && line_right.read){
-		// 	// Maybe rotate in arbitrary direction
-		// } else if(line_left.read && line_middle.read && !line_right.read){
-		// 	// Turn right
-		// } else if(line_left.read && !line_middle.read && line_right.read){
-		// 	// Move forward
-		// 	Motor_Run(MOTORA, FORWARD, 100);
-		// 	Motor_Run(MOTORB, FORWARD, 100);
-		// } else if(line_left.read && !line_middle.read && !line_right.read){
-		// 	// Rotate right
-		// } else if(!line_left.read && line_middle.read && line_right.read){
-		// 	// Turn left
-		// } else if(!line_left.read && line_middle.read && !line_right.read){
-		// 	// Odd case. Probably stop until sensors read properly.
-		// } else if(!line_left.read && !line_middle.read && line_right.read){
-		// 	// Rotate left.
-		// } else if(!line_left.read && !line_middle.read && !line_right.read){
-		// 	// Either wait for sensors to read properly or rotate in arbitrary direction.
-		// }
-		// printf("Line left read: %i\n", line_left.read);
-		// printf("Line middle read: %i\n", line_middle.read);
-		// printf("Line right read: %i\n", line_right.read);
+		if(line_left.read && line_middle.read && line_right.read){
+			// Maybe rotate in arbitrary direction
+		} else if(line_left.read && line_middle.read && !line_right.read){
+			// Turn right
+		} else if(line_left.read && !line_middle.read && line_right.read){
+			// Move forward
+			Motor_Run(MOTORA, FORWARD, 100);
+			Motor_Run(MOTORB, FORWARD, 100);
+		} else if(line_left.read && !line_middle.read && !line_right.read){
+			// Rotate right
+		} else if(!line_left.read && line_middle.read && line_right.read){
+			// Turn left
+		} else if(!line_left.read && line_middle.read && !line_right.read){
+			// Odd case. Probably stop until sensors read properly.
+		} else if(!line_left.read && !line_middle.read && line_right.read){
+			// Rotate left.
+		} else if(!line_left.read && !line_middle.read && !line_right.read){
+			// Either wait for sensors to read properly or rotate in arbitrary direction.
+		}
+		printf("Line left read: %i\n", line_left.read);
+		printf("Line middle read: %i\n", line_middle.read);
+		printf("Line right read: %i\n", line_right.read);
 		usleep(500000);
 	}
 
-	// pthread_join(obstacle_thread, NULL);
-	// pthread_join(line_left_thread, NULL);
-	// pthread_join(line_middle_thread, NULL);
-	// pthread_join(line_right_thread, NULL);
+	pthread_join(obstacle_thread, NULL);
+	pthread_join(line_left_thread, NULL);
+	pthread_join(line_middle_thread, NULL);
+	pthread_join(line_right_thread, NULL);
   	
 	DEV_ModuleExit();
 

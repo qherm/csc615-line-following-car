c82ca13539c19a3c6db9938099a0e161e2b4f1a9
qherm <shanewaxler1@gmail.com>
2022-05-09 17:22:05 -0700

    fixed sighandler :)

--- main/main.c
+++ main/main.c
@@ -13,13 +13,12 @@ void Handler(int signo)
 
 int main()
 {
-  	signal(SIGINT, Handler);
 	if(gpioInitialise() < 0)
 	{
 		fprintf(stderr, "pigpio initialise failure\n");
 		return 1;
 	}
-	
+  signal(SIGINT, Handler);	
 	gpioSetMode(IRL, PI_INPUT);
   	gpioSetMode(IRM, PI_INPUT);
 	gpioSetMode(IRR, PI_INPUT);
@@ -58,25 +57,25 @@ int main()
 	{
 		// LineSensor.read==1: sensor reads white
 		// LineSensor.read==0: sensor reads black
-		if(line_left.read && line_middle.read && line_right.read){
-			// Maybe rotate in arbitrary direction
-		} else if(line_left.read && line_middle.read && !line_right.read){
-			// Turn right
-		} else if(line_left.read && !line_middle.read && line_right.read){
-			// Move forward
-			Motor_Run(MOTORA, FORWARD, 100);
-			Motor_Run(MOTORB, FORWARD, 100);
-		} else if(line_left.read && !line_middle.read && !line_right.read){
-			// Rotate right
-		} else if(!line_left.read && line_middle.read && line_right.read){
-			// Turn left
-		} else if(!line_left.read && line_middle.read && !line_right.read){
-			// Odd case. Probably stop until sensors read properly.
-		} else if(!line_left.read && !line_middle.read && line_right.read){
-			// Rotate left.
-		} else if(!line_left.read && !line_middle.read && !line_right.read){
-			// Either wait for sensors to read properly or rotate in arbitrary direction.
-		}
+		// if(line_left.read && line_middle.read && line_right.read){
+		// 	// Maybe rotate in arbitrary direction
+		// } else if(line_left.read && line_middle.read && !line_right.read){
+		// 	// Turn right
+		// } else if(line_left.read && !line_middle.read && line_right.read){
+		// 	// Move forward
+		// 	Motor_Run(MOTORA, FORWARD, 100);
+		// 	Motor_Run(MOTORB, FORWARD, 100);
+		// } else if(line_left.read && !line_middle.read && !line_right.read){
+		// 	// Rotate right
+		// } else if(!line_left.read && line_middle.read && line_right.read){
+		// 	// Turn left
+		// } else if(!line_left.read && line_middle.read && !line_right.read){
+		// 	// Odd case. Probably stop until sensors read properly.
+		// } else if(!line_left.read && !line_middle.read && line_right.read){
+		// 	// Rotate left.
+		// } else if(!line_left.read && !line_middle.read && !line_right.read){
+		// 	// Either wait for sensors to read properly or rotate in arbitrary direction.
+		// }
 		// printf("Line left read: %i\n", line_left.read);
 		// printf("Line middle read: %i\n", line_middle.read);
 		// printf("Line right read: %i\n", line_right.read);

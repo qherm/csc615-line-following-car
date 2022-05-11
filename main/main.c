4ccee2e9992518ef58073335ee8c6e19f6759101
ShaneWaxler1 <swaxler@mail.sfsu.edu>
2022-05-10 17:00:25 -0700

    new thread instantiation test

--- main/main.c
+++ main/main.c
@@ -58,24 +58,19 @@ int main()
 
 	pthread_t line_left_thread, line_middle_thread, line_right_thread,  start_stop_button_thread, object_middle_thread;
 	int line_left_return, line_middle_return, line_right_return, start_stop_button_return, object_middle_return;
-
 	sensor line_right, line_middle, line_left, object_middle, start_stop_button;
+	sensor sensors[5] = [line_right, line_middle, line_left, object_middle, start_stop_button];
+
+	for(int i = 0; i < 5; i++)
+	{
+		sensors[i].read = 0;
+		sensors[i].cont = true;
+	}
 
 	line_left.pin = IRL;
-	line_left.read = 0;
-	line_left.cont = true;
-
 	line_middle.pin = IRM;
-	line_middle.read = 0;
-	line_middle.cont = true;
-
 	line_right.pin = IRR;
-	line_right.read = 0;
-	line_right.cont = true;
-
 	start_stop_button.pin = BUTTON_PIN;
-	start_stop_button.read = 0;
-	start_stop_button.cont = true;
 
 	line_left_return = pthread_create(&line_left_thread, NULL, sense, &line_left);
 	line_middle_return = pthread_create(&line_middle_thread, NULL, sense, &line_middle);
@@ -100,59 +95,18 @@ int main()
 
 	printf("GO!\n"); // 1. Prints properly
 	
-	while(!start_stop_button.read)
-	{
-		// LineSensor.read==1: sensor reads white
-		// LineSensor.read==0: sensor reads black
-		if(line_left.read && line_middle.read && line_right.read){
-			// Maybe rotate in arbitrary direction
-			Motor_Run(LEFT_MOTOR, FORWARD, 0);
-			Motor_Run(RIGHT_MOTOR, FORWARD, 0);
-		} else if(line_left.read && line_middle.read && !line_right.read){
-			// Turn right
-			Motor_Run(LEFT_MOTOR, FORWARD, 100);
-			Motor_Run(RIGHT_MOTOR, FORWARD, 0);
-		} else if(line_left.read && !line_middle.read && line_right.read){
-			// Move forward
-			Motor_Run(LEFT_MOTOR, FORWARD, 100);
-			Motor_Run(RIGHT_MOTOR, FORWARD, 100);
-		} else if(line_left.read && !line_middle.read && !line_right.read){
-			// Rotate right
-			Motor_Run(LEFT_MOTOR, FORWARD, 100);
-			Motor_Run(RIGHT_MOTOR, BACKWARD, 100);
-		} else if(!line_left.read && line_middle.read && line_right.read){
-			// Turn left
-			Motor_Run(LEFT_MOTOR, FORWARD, 0);
-			Motor_Run(RIGHT_MOTOR, FORWARD, 100);
-		} else if(!line_left.read && line_middle.read && !line_right.read){
-			// Odd case. Probably stop until sensors read properly.
-			Motor_Run(LEFT_MOTOR, FORWARD, 0);
-			Motor_Run(RIGHT_MOTOR, FORWARD, 0);
-		} else if(!line_left.read && !line_middle.read && line_right.read){
-			// Rotate left.
-			Motor_Run(LEFT_MOTOR, BACKWARD, 100);
-			Motor_Run(RIGHT_MOTOR, FORWARD, 100);
-		} else if(!line_left.read && !line_middle.read && !line_right.read){
-			// Either wait for sensors to read properly or rotate in arbitrary direction.
-			Motor_Run(LEFT_MOTOR, FORWARD, 0);
-			Motor_Run(RIGHT_MOTOR, FORWARD, 0);
-		}
-	}
+	driving_logic(&line_left, &line_middle, &line_right, &start_stop_button);
 
-  	printf("I have reached this print statement\n"); // 3. Does not print
 	line_left.cont = false;
 	line_middle.cont = false;
 	line_right.cont = false;
 	start_stop_button.cont = false;
+	
 	// pthread_join(obstacle_thread, NULL);
 	pthread_join(line_left_thread, NULL);
-	printf("HERE 1");
 	pthread_join(line_middle_thread, NULL);
-	printf("HERE 2");
 	pthread_join(line_right_thread, NULL);
-	printf("HERE 3");
   	pthread_join(start_stop_button_thread, NULL);
-	printf("HERE 4");
   	
 	printf("stopping\n");
 	Motor_Stop(MOTORA);
